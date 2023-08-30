26e0029e063ffa658f4b64fcb1fd98fd645ed598
tcarpenter7699 <tcarpenter7699@gmail.com>
2023-08-30 16:42:41 -0500

    first commit

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/*NAVBAR JS*/
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
 
@@ -5,3 +6,25 @@ menu.addEventListener('click', function() {
    menu.classList.toggle('is-active');
    menuLinks.classList.toggle('active');
 });
+
+/*SLIDESHOW JS*/
+let slideIndex = [1,1,1];
+let slideId = ["mySlides1", "mySlides2", "mySlides3"]
+showSlides(1,0);
+showSlides(1,1);
+showSlides(1,2);
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+function showSlides(n, no) {
+  let i;
+  let x= document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no]= 1}
+  if (n < 2) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+x[slideIndex[no]-1].style.display = "block";
+}
